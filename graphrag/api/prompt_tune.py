c177d414a45082a463f097795daf93e3eb322642
Asaf Atzmon <asaf.atzmon@gmail.com>
2025-01-09 17:01:04 +0200

    refactor: Convert generate_indexing_prompts to synchronous function

--- graphrag/api/prompt_tune.py
+++ graphrag/api/prompt_tune.py
@@ -46,7 +46,7 @@ from graphrag.prompt_tune.types import DocSelectionType
 
 
 @validate_call
-async def generate_indexing_prompts(
+def generate_indexing_prompts(
     config: GraphRagConfig,
     root: str,
     chunk_size: PositiveInt = MIN_CHUNK_SIZE,
