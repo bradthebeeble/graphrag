9714dd6fe6db65071fb585495dd8fb4b24bf958f
Asaf Atzmon <asaf.atzmon@gmail.com>
2025-01-23 16:01:23 +0200

    refactor: Update call_db function to use static query and improve data handling

--- graphrag/chat/main.py
+++ graphrag/chat/main.py
@@ -153,8 +153,8 @@ def call_grid(state: ExtendedMessagesState):
 
 def call_db(state: ExtendedMessagesState):
     global openai_api_key
-    if not state.get("generated_db_query"):
-        return
+    # if not state.get("generated_db_query"):
+        # return
     graph = Neo4jGraph(url=neo4j_config.uri,
                         username=neo4j_config.username,
                         password=neo4j_config.password,
@@ -167,14 +167,18 @@ def call_db(state: ExtendedMessagesState):
           return_direct=True,
     )
     response = chain.invoke({
-        "query" : state["generated_db_query"]
+        "query" : "show 5 dealership venues" # state["generated_db_query"]
     })
     import pandas as pd
 
     result_data = [item['dv'] for item in response["result"]]
-    df = pd.DataFrame(result_data)
-    row_header = [key for key in result_data[0].keys() if key not in {'title', 'human_readable_id', 'dirty', 'id'}]
-    print(df)
+    row_headers = [key for key in result_data[0].keys() if key not in {'title', 'human_readable_id', 'dirty', 'id'}]
+    # remove from result_data, all the key/values that key is one of {'title', 'human_readable_id', 'dirty', 'id'}. AI!
+    transformed_data =  {key: [row[key] for row in result_data] for key in result_data[0].keys()}
+    df = pd.DataFrame(transformed_data)
+    formatted_table = pd.DataFrame({row: df[row].values for row in row_headers}, index=row_headers)
+    formatted_table.columns = df["title"]
+    print(formatted_table)
 
 def call_candidate_fup_questions(state: ExtendedMessagesState):
     is_tool_message = isinstance(state["messages"][-2], ToolMessage) if len(state["messages"]) > 1 else False
@@ -281,13 +285,14 @@ workflow.add_node("call_grid", call_grid)
 workflow.add_node("call_db", call_db)
 workflow.add_node("call_retrieve_candidate_dimensions", call_retrieve_candidate_dimensions)
 workflow.add_node("call_fup_candidate_questions", call_candidate_fup_questions)
-workflow.add_conditional_edges(START, command_router)
+# workflow.add_conditional_edges(START, command_router)
 workflow.add_conditional_edges("call_model", route_tools)
 workflow.add_edge("call_tools", "call_model")
 workflow.add_edge("call_fup_candidate_questions", "call_retrieve_candidate_dimensions")
 workflow.add_edge("call_retrieve_candidate_dimensions", "display_results")
 workflow.add_edge("call_grid", "call_db")
-workflow.add_edge("call_db", "display_results")
+workflow.add_edge(START, "call_db") # test
+# workflow.add_edge("call_db", "display_results")
 
 
 
